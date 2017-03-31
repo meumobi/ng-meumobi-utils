f1a06e46cb8e556ff1cbfc9f3c83ec235ef6f586
victor dias <victor.dias@meumobi.com>
2017-03-31 14:52:23 -0300

    UPGRADE: Closes #9, google-analytics-plugin upgrade

--- src/lib/Cordova/Analytics.js
+++ src/lib/Cordova/Analytics.js
@@ -3,7 +3,6 @@
   
 	angular
 	.module('ngMeumobi.Cordova.analytics', [])
-	.factory('googleAnalyticsCordova', googleAnalyticsCordova)
 	.factory('googleAnalytics', googleAnalytics)
   .factory('$cordovaGoogleAnalytics', $cordovaGoogleAnalytics)
 	.factory('meuAnalytics', analytics);
@@ -15,125 +14,70 @@
     install   :     cordova plugin add https://github.com/danwilson/google-analytics-plugin.git
   */
 
-  function $cordovaGoogleAnalytics($q, $window, $log) {
+  function $cordovaGoogleAnalytics($q, $window, $log, $exceptionHandler) {
     
     var service = {};
     
     service.startTrackerWithId = startTrackerWithId;
-    service.debugMode = debugMode;
     service.trackView = trackView;
     service.trackEvent = trackEvent;
-    service.setUserId = setUserId;
     
     return service;
 
-    function setUserId(id) {
-      var d = $q.defer();
-
-      $log.debug('Set User Id: ' + id);
-      
-      $window.analytics.setUserId(id, function (response) {
-        d.resolve(response);
-      }, function (error) {
-        d.reject(error);
-      });
-
-      return d.promise;
-    }
 
     function startTrackerWithId(id) {
-      var d = $q.defer();
-
-      $log.debug('Start tracking GA Id: ' + id);
-
-      $window.analytics.startTrackerWithId(id, function (response) {
-        d.resolve(response);
-      }, function (error) {
-        d.reject(error);
+      return $q(function(resolve, reject) {
+        try {
+          $window.ga.startTrackerWithId(id, 10);
+          $log.debug('Start tracking GA Id: ' + id);
+          resolve();
+        } catch (e) {
+          $exceptionHandler(e);
+          reject(e);
+        };
       });
-
-      return d.promise;
-    }
-
-    function debugMode() {
-      var d = $q.defer();
-
-      $window.analytics.debugMode(function (response) {
-        d.resolve(response);
-      }, function () {
-        d.reject();
-      });
-
-      return d.promise;
     }
 
     function trackView(screenName) {
-      var d = $q.defer();
-      
-      $log.debug('Track View: ' + screenName);
-      
-      $window.analytics.trackView(screenName, function (response) {
-        d.resolve(response);
-      }, function (error) {
-        d.reject(error);
+      return $q(function(resolve, reject) {
+        try {
+          $window.ga.trackView(screenName);
+          $log.debug('Track View: ' + screenName);
+          resolve();
+        } catch (e) {
+          $exceptionHandler(e);
+          reject(e);
+        };
       });
-
-      return d.promise;
     }
 
     function trackEvent(category, action, label, value) {
-      var d = $q.defer();
-
-      var ev = [category, action, label, value];
-      $log.debug('Track Event: ' + ev.toString());
-      
-      $window.analytics.trackEvent(category, action, label, value, function (response) {
-        d.resolve(response);
-      }, function (error) {
-        d.reject(error);
+      return $q(function(resolve, reject) {
+        try {
+          var ev = [category, action, label, value];
+          $window.ga.trackEvent(category, action, label, value);
+          $log.debug('Track Event: ' + ev.toString());
+          resolve();
+        } catch (e) {
+          $exceptionHandler(e);
+          reject(e);
+        };
       });
-
-      return d.promise;
     }
   }
-		
-	function googleAnalyticsCordova($cordovaGoogleAnalytics, $log) {
-    
-    var service = {};
-    
-    service.init = init;
-    service.debugMode = $cordovaGoogleAnalytics.debugMode;
-    service.trackView = $cordovaGoogleAnalytics.trackView;
-    service.trackEvent = $cordovaGoogleAnalytics.trackEvent;
-    service.startTrackerWithId = $cordovaGoogleAnalytics.startTrackerWithId;
-    service.setUserId = $cordovaGoogleAnalytics.setUserId;
-    
-    return service;
-    
-    function init(trackId) {
-      $cordovaGoogleAnalytics.startTrackerWithId(trackId);
-    }
-	}
   
 	function googleAnalytics($log, $q) {
     
     var service = {};
     
-    service.init = init;
-    service.debugMode = debugMode;
     service.trackView = trackView;
     service.trackEvent = trackEvent;
     service.startTrackerWithId  = startTrackerWithId;
-    service.setUserId = setUserId;
     
     return service;
     
     function init(trackId) {
       $log.debug('Google Analytics track Id: ' + trackId);
-    }
-    
-    function debugMode() {
-      $log.debug('Enable Debug');
     }
     
     function trackView(title) {
@@ -152,16 +96,12 @@
 
       return d.promise;
     }
-    
-    function setUserId(id) {
-      $log.debug('Tracking User: ' + id);
-    }
 	}
   
 	function analytics($injector, $window) {
 
 		if ($window.cordova) {
-			return $injector.get('googleAnalyticsCordova');
+			return $injector.get('$cordovaGoogleAnalytics');
 		} else {
 		  return $injector.get('googleAnalytics');
 		}
